9ebec4618ed8a318fd6f651d2bb6304c525a1032
Diane Magana <dianemagana@outlook.com>
2020-11-30 23:59:31 -0800

    Trying to make app work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
-import Directory from "./components/Directory";
-import './App.css';
+import React, { useEffect, useState } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
+import { getUsers } from "./API";
+import "./App.css";
 
-const App = () => {
+function App() {
+  const [initialUsers, updateAvailableUsers] = useState([]);
+  const [usersToRender, updateUsersToRender] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
+  }, []);
 
   return (
-
     <div className="App">
-      <Directory />
+      <h1>Employee Directory</h1>
+      <p>
+        To filter by first name, please begin your search in the user input
+        below
+      </p>
+      <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
+      <Table users={usersToRender} />
     </div>
-
-  )
-
+  );
 }
 
 export default App;
